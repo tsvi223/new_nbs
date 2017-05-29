2cd391197c6d3c0eb690f5889c545f42f367531c
tsvi <tsvi223@gmail.com>
2017-05-29 19:17:28 +0300

    angular - numbers only input , general - refactoring

--- server/app.js
+++ server/app.js
@@ -1,31 +1,15 @@
 var express = require("express");
 var path = require("path");
-
 var port =   '8080';
 var app = express();
 var http = require('http').createServer(app);
-console.log('here');
-var bodyParser      = require('body-parser');
+var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-
-
-
-
-
-
-
-
-
-
-
 app.use('/app', express.static(path.join(__dirname, '../app')))
-console.log(path.join( __dirname, '../app'));
 app.use('/assets', express.static(path.join(__dirname, '../assets')))
 
 require('./route')(app);
-
 
 http.listen(port ,function(){
     console.log('Starting Web Server on Port   ' + port );
--- server/model/check-limited.js
+++ server/model/check-limited.js
@@ -2,25 +2,23 @@ var DBAccountLimited = require('../db/accounts-limited');
 var request = require('./req');
 var xray = require('x-ray');
 var q = require('q');
+var strings = require('./strings');
 
-
-function checkLimitedScrapingAccount(bank , branch , acount_number){  console.log(bank , branch , acount_number);
+function checkLimitedScrapingAccount(bank , branch , acount_number){
     var d = q.defer();
-    var url = 'http://www.boi.org.il/_layouts/boi/handlers/WebPartHandler.aspx?wp=RestrictedAccountsSearch&lang=en&' +
-              'Bank=' + bank + '&Branch=' + branch + '&Account=' + acount_number;
-    request.action('GET' , url).then(function(html){
+    var url = strings.url_scraping_account + 'Bank=' + bank + '&Branch=' + branch + '&Account=' + acount_number;
+    request.action('GET' , url).then(function(html){ 
         var x = xray();
-        x(html ,'div .BoiRestrictedAccountsRestricted' , { txt : '@html' }  )(function(err , obj){ console.log(obj);
+        x(html , strings.xray.selector_account , { txt : '@html' }  )(function(err , obj){
             if(!obj.txt) return  d.resolve({status : 'correct'});
             var result_xray = obj.txt.trim();
-            if(result_xray == 'The account number you have entered is not on the list of restricted accounts'){         console.log('result if :: ');
+            if(result_xray == strings.xray.string_compare_account){
               var result = {
                 status : 'correct'
               }
             }
             else{
               var expired = result_xray.replace(/Restricted account until|\r|\n|\s|/g, "").trim();
-              console.log(expired);
               var result = {
                 status : 'limited',
                 expired : expired
@@ -28,24 +26,21 @@ function checkLimitedScrapingAccount(bank , branch , acount_number){  console.lo
             }
             return d.resolve(result);
         })
-
     })
     return d.promise;
 }
 
 function checkLimitedScrapingPersonal(id){
     var d = q.defer();
-    var url = 'http://www.boi.org.il/_layouts/boi/handlers/WebPartHandler.aspx?wp=RestrictedAccountsSearch' +
-                '&lang=en&Company=' + id
-
+    var url = strings.url_scraping_personal + id;
     request.action('GET' , url).then(function(html){
         var x = xray();
-        x(html ,'div .BoiRestrictedCircumstancesCaseResult div' , { txt : '@html' }  )(function(err , obj){ console.log(obj);
+        x(html , strings.xray.selector_personal , { txt : '@html' }  )(function(err , obj){
             var txt =  obj.txt.trim().replace(/Number  does|<span class="BoiRestrictedCircumstancesCaseId"><\/span>/g , '')
-            if(txt.indexOf('not appear on the list') == -1)
-                return d.resolve('limited')
+            if(txt.indexOf(strings.xray.string_compare_persoanl) == -1)
+                return d.resolve('limited');
             else
-                return d.resolve('correct')
+                return d.resolve('correct');
         })
 
     })
@@ -58,8 +53,7 @@ function checkLimitedDB(bank , branch , acount_number){
         branch : branch,
         account : acount_number
     }
-    console.log(account);
-    return DBAccountLimited.checkAccounts(account).then(function(result){ console.log(result);
+    return DBAccountLimited.checkAccounts(account).then(function(result){
          var status = '';
          if(!result) status = 'correct';
          if(result) status = 'limited';
@@ -67,23 +61,23 @@ function checkLimitedDB(bank , branch , acount_number){
     })
 }
 
-module.exports.action = function(req , res , next){ console.log(req.params.action);
+module.exports.action = function(req , res , next){
     switch (req.params.action) {
         case 'sql' : {
             checkLimitedDB(req.body.bank , req.body.branch , req.body.account).then(function(status){
-                return res.json({ status : status })
+                return res.json({ status : status });
             })
         }
         break;
         case 'scrape-account' : {
             checkLimitedScrapingAccount(req.body.bank , req.body.branch , req.body.account).then(function(status){
-                return res.json(status)
+                return res.json(status);
             })
         }
         break;
         case 'scrape-personal' : {
             checkLimitedScrapingPersonal(req.body.id).then(function(status){
-                return res.json({ status : status })
+                return res.json({ status : status });
             })
         }
         break;
